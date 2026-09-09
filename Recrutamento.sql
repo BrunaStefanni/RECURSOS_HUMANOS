8edf38e1fd819fa6566fae7123c118c4997162d2
Tatiane Aragão <thaty.arquitetura@gmail.com>
2026-09-09 11:29:53 +0100

    última atualização: Tabela vaga

--- Recrutamento.sql
+++ Recrutamento.sql
@@ -4,19 +4,54 @@ USE refirmas;
 //Atualmente o nome da empresa está como refirmas, mas será alterado futuramente//
 
 CREATE TABLE candidato(
-
+id_candidato INT PRIMARY KEY,
+nome_candidato VARCHAR (50),
+email VARCHAR (50),
+telefone INT,
+data_nascimento DATE,
+data_registro DATE,
 );
 
 CREATE TABLE empresa(
-
+id_empresa INT PRIMARY KEY,
+nome_empresa VARCHAR (50),
+nif INT,
+email VARCHAR (50),
+telefone INT,
+website VARCHAR (100),
 );
 
 CREATE TABLE recrutador(
+id_recrutador INT PRIMARY KEY,
+nome_recrutador VARCHAR (50),
+email VARCHAR (50),
+telefone INT,
+id_empresa INT,
+FK
+);
 
+CREATE TABLE localizacao(
+id_localizacao INT PRIMARY KEY,
+cidade VARCHAR (50),
+distrito VARCHAR (50),
+pais VARCHAR (50),
 );
 
 CREATE TABLE vaga(
-
+id_vaga INT PRIMARY KEY,
+titulo VARCHAR (50)
+descricao VARCHAR (200),
+data_publicacao DATE,
+data_limite DATE,
+salario_min DECIMAL (10,2),
+salario_max  DECIMAL (10,2),
+tipo_contrato VARCHAR (50),
+id_empresa INT,
+FK
+id_recrutador INT,
+FK
+id_localizacao INT,
+FK
 );
 
 CREATE TABLE candidatura(
@@ -44,10 +79,6 @@ CREATE TABLE experiencia_profissional(
 );
 
 CREATE TABLE formacao(
-
-);
-
-CREATE TABLE localizacao(
 
 );
 
