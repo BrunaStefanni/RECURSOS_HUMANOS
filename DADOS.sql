eef48c89397fe7d62a905c1a3104dde4fa79946c
Bruna Leite <edastefanni@hotmail.com>
2026-09-09 11:24:13 +0100

    dados

--- DADOS.sql
+++ DADOS.sql
@@ -1,60 +1,27 @@
 CREATE DATABASE refirmas;
-USE refirmas;
-
 //Atualmente o nome da empresa está como refirmas, mas será alterado futuramente//
-
 CREATE TABLE candidato(
-
-);
-
 CREATE TABLE empresa(
-
-);
-
 CREATE TABLE recrutador(
-
-);
-
 CREATE TABLE vaga(
-
-);
-
 CREATE TABLE candidatura(
-
-);
-
 CREATE TABLE entrevista(
-
-);
-
 CREATE TABLE qualificacao(
-
-);
-
 CREATE TABLE candidato_qualificacao(
-
-);
-
 CREATE TABLE vaga_qualificacao(
-
-);
-
 CREATE TABLE experiencia_profissional(
-
-);
-
 CREATE TABLE formacao(
-
-);
-
 CREATE TABLE localizacao(
-
-);
-
 CREATE TABLE estado_candidatura(
-
-);
-
 CREATE TABLE historico_candidatura(
+»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
 
-);
+INSERT INTO candidato ()
+
+INSERT INTO empresa ()
+
+INSERT INTO recrutador ()
+
+INSERT INTO vaga ()
+
+INSERT INTO candidatura ()
--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # RECURSOS_HUMANOS
 Repositório para desenvolvimento de projeto para trabalho do curso TDS. 
 
-## Disciplina de Base de Dados SQL, descrições:
+## INSERT DADOS
 
-Este trabalho é uma base de dados destinada a apoiar empresas no processo de recrutamento e seleção de candidatos.		
-O sistema permite:		
-registar empresas;		
-registar candidatos;		
-registar recrutadores;		
-criar e gerir ofertas de emprego;		
-associar competências às vagas;		
-registar competências dos candidatos;		
-registar formação académica;		
-registar experiência profissional;		
-gerir candidaturas;		
-acompanhar entrevistas;		
-controlar o estado das candidaturas;		
-analisar salários e processos de recrutamento.		
+Apenas contribuição de dados para a tabela
