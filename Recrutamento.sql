2cddf263ccea40cc84a5cc0db80a52d900d238b3
Tatiane Aragão <thaty.arquitetura@gmail.com>
2026-09-15 15:27:07 +0100

    m

--- Recrutamento.sql
+++ Recrutamento.sql
@@ -10,6 +10,7 @@ email VARCHAR (50),
 telefone INT,
 data_nascimento DATE,
 data_registro DATE
+morada VARCHAR (100)
 );
 
 CREATE TABLE empresa(
@@ -18,6 +19,7 @@ nome_empresa VARCHAR (50),
 nif INT,
 email VARCHAR (50),
 telefone INT,
+morada VARCHAR (100),
 website VARCHAR (100)
 );
 
@@ -27,7 +29,7 @@ nome_recrutador VARCHAR (50),
 email VARCHAR (50),
 telefone INT,
 id_empresa INT,
-FK
+FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
 );
 
 CREATE TABLE localizacao(
@@ -39,7 +41,7 @@ pais VARCHAR (50)
 
 CREATE TABLE vaga(
 id_vaga INT PRIMARY KEY AUTO_INCREMENT,
-titulo VARCHAR (50)
+titulo VARCHAR (50),
 descricao VARCHAR (200),
 data_publicacao DATE,
 data_limite DATE,
@@ -47,11 +49,11 @@ salario_min DECIMAL (10,2),
 salario_max  DECIMAL (10,2),
 tipo_contrato VARCHAR (50),
 id_empresa INT,
-FK
+FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
 id_recrutador INT,
-FK
+FOREIGN KEY (id_recrutador) REFERENCES recrutador(id_recrutador),
 id_localizacao INT,
-FK
+FOREIGN KEY (id_localizacao) REFERENCES localizacao(id_localizacao)
 );
 
 CREATE TABLE estado_candidatura(
@@ -65,11 +67,11 @@ data_candidatura DATE,
 salario_pretendido DECIMAL (10,2),
 carta_apresentacao VARCHAR (500),
 id_candidato INT,
-FK
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 id_vaga INT,
-FK
+FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
 id_estado INT,
-FK
+FOREIGN KEY (id_estado) REFERENCES estado_candidatura(id_estado)
 );
 
 CREATE TABLE entrevista(
@@ -79,7 +81,7 @@ tipo VARCHAR (50),
 observacoes VARCHAR (50),
 avaliacao INT,
 id_candidatura INT,
-FK
+FOREIGN KEY (id_candidatura) REFERENCES candidatura(id_candidatura)
 );
 
 CREATE TABLE qualificacao(
@@ -90,24 +92,29 @@ descricao VARCHAR (500)
 
 CREATE TABLE candidato_qualificacao(
 id_candidato INT,
-FK
+FOREIGN KEY (id_candidato) REFERENCES candidato (id_candidato),
 id_qualificacao INT,
-FK
+FOREIGN KEY (id_qualificacao) REFERENCES qualificacao (id_qualificacao),
 nivel VARCHAR (50)
+PRIMARY KEY (id_candidato, id_qualificacao)
 );
+
+// O CHAT sugeriu que houvesse uma PK composta entre id_candidato e id_qualificacao//
+// O código então ficaria assim: PRIMARY KEY (id_candidato, id_qualificacao) //
 
 CREATE TABLE vaga_qualificacao(
 id_vaga INT,
-FK
+FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
 id_qualificacao INT,
-FK
-nivel_minimo VARCHAR (50)
+FOREIGN KEY (id_qualificacao) REFERENCES qualificacao(id_qualificacao),
+nivel_minimo VARCHAR (50),
+PRIMARY KEY (id_vaga, id_qualificacao)
 );
 
 CREATE TABLE experiencia_profissional(
 id_experiencia INT PRIMARY KEY AUTO_INCREMENT,
 id_candidato INT,
-FK
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 empresa VARCHAR (50),
 data_inicio DATE,
 data_fim DATE,
@@ -117,7 +124,7 @@ descricao VARCHAR (500)
 CREATE TABLE formacao(
 id_formacao INT PRIMARY KEY AUTO_INCREMENT,
 id_candidato INT,
-FK
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 instituicao VARCHAR (50),
 curso VARCHAR (50),
 grau INT,
@@ -128,9 +135,9 @@ data_fim DATE
 CREATE TABLE historico_candidatura(
 id_historico INT PRIMARY KEY AUTO_INCREMENT,
 id_candidatura INT,
-FK
+FOREIGN KEY (id_candidatura) REFERENCES candidatura(id_candidatura),
 id_estado INT,
-FK
+FOREIGN KEY (id_estado) REFERENCES estado_candidatura(id_estado),
 data_alteracao DATE,
 observacao VARCHAR (100)
 );
