7ade3154c37dc2dac6d7388bd18048495b9815cb
Bruna Leite <edastefanni@hotmail.com>
2026-09-16 18:30:09 +0100

    adc arquivo com cod final do database

--- Cod_database_next_people.sql
+++ Cod_database_next_people.sql
@@ -1,17 +1,15 @@
-/* apenas rascunho  */
+CREATE DATABASE next_people;
+USE next_people;
 
-CREATE DATABASE refirmas;
-USE refirmas;
-
-//Atualmente o nome da empresa está como refirmas, mas será alterado futuramente//
+/* Criação de base de dados para trabalho de SQL - Empresa de RH Next People */
 
 CREATE TABLE candidato(
 id_candidato INT PRIMARY KEY AUTO_INCREMENT,
 nome_candidato VARCHAR (50),
 email VARCHAR (50),
-telefone INT,
+telefone VARCHAR (50),
 data_nascimento DATE,
-data_registro DATE
+data_registro DATE,
 morada VARCHAR (100)
 );
 
@@ -20,7 +18,7 @@ id_empresa INT PRIMARY KEY AUTO_INCREMENT,
 nome_empresa VARCHAR (50),
 nif INT,
 email VARCHAR (50),
-telefone INT,
+telefone VARCHAR (50),
 morada VARCHAR (100),
 website VARCHAR (100)
 );
@@ -29,7 +27,7 @@ CREATE TABLE recrutador(
 id_recrutador INT PRIMARY KEY AUTO_INCREMENT,
 nome_recrutador VARCHAR (50),
 email VARCHAR (50),
-telefone INT,
+telefone VARCHAR (50),
 id_empresa INT,
 FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
 );
@@ -51,10 +49,11 @@ salario_min DECIMAL (10,2),
 salario_max  DECIMAL (10,2),
 tipo_contrato VARCHAR (50),
 id_empresa INT,
-FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
 id_recrutador INT,
-FOREIGN KEY (id_recrutador) REFERENCES recrutador(id_recrutador),
 id_localizacao INT,
+
+FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
+FOREIGN KEY (id_recrutador) REFERENCES recrutador(id_recrutador),
 FOREIGN KEY (id_localizacao) REFERENCES localizacao(id_localizacao)
 );
 
@@ -69,10 +68,11 @@ data_candidatura DATE,
 salario_pretendido DECIMAL (10,2),
 carta_apresentacao VARCHAR (500),
 id_candidato INT,
-FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 id_vaga INT,
-FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
 id_estado INT,
+
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
+FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
 FOREIGN KEY (id_estado) REFERENCES estado_candidatura(id_estado)
 );
 
@@ -93,57 +93,55 @@ descricao VARCHAR (500)
 );
 
 CREATE TABLE candidato_qualificacao(
-id_candidato INT,
-FOREIGN KEY (id_candidato) REFERENCES candidato (id_candidato),
-id_qualificacao INT,
-FOREIGN KEY (id_qualificacao) REFERENCES qualificacao (id_qualificacao),
-nivel VARCHAR (50)
-PRIMARY KEY (id_candidato, id_qualificacao)
+id_candidato_qualificacao INT PRIMARY KEY AUTO_INCREMENT,
+id_candidato INT, 
+id_qualificacao INT, 
+nivel VARCHAR (50),
+
+FOREIGN KEY (id_candidato) REFERENCES candidato (id_candidato), 
+FOREIGN KEY (id_qualificacao) REFERENCES qualificacao (id_qualificacao)
 );
 
-/* O código então ficaria assim: PRIMARY KEY (id_candidato, id_qualificacao), MAS... */
-/*FORAM ALTERADAS AS DUAS TABELAS VAGA_QUALIFICACAO E CANDIDATO_QUALIFICACAO PARA UMA ESTRUTURA MAIS SIMPLES */
-
 CREATE TABLE vaga_qualificacao(
+id_vaga_qualificacao INT PRIMARY KEY AUTO_INCREMENT,
 id_vaga INT,
-FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
 id_qualificacao INT,
-FOREIGN KEY (id_qualificacao) REFERENCES qualificacao(id_qualificacao),
 nivel_minimo VARCHAR (50),
-PRIMARY KEY (id_vaga, id_qualificacao)
+
+FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga),
+FOREIGN KEY (id_qualificacao) REFERENCES qualificacao(id_qualificacao)
 );
 
 CREATE TABLE experiencia_profissional(
 id_experiencia INT PRIMARY KEY AUTO_INCREMENT,
 id_candidato INT,
-FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 empresa VARCHAR (50),
 data_inicio DATE,
 data_fim DATE,
-descricao VARCHAR (500)
+descricao VARCHAR (500),
+
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato)
 );
 
 CREATE TABLE formacao(
 id_formacao INT PRIMARY KEY AUTO_INCREMENT,
 id_candidato INT,
-FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato),
 instituicao VARCHAR (50),
 curso VARCHAR (50),
 grau INT,
 data_inicio DATE,
-data_fim DATE
+data_fim DATE,
+
+FOREIGN KEY (id_candidato) REFERENCES candidato(id_candidato)
 );
 
 CREATE TABLE historico_candidatura(
 id_historico INT PRIMARY KEY AUTO_INCREMENT,
 id_candidatura INT,
-FOREIGN KEY (id_candidatura) REFERENCES candidatura(id_candidatura),
 id_estado INT,
-FOREIGN KEY (id_estado) REFERENCES estado_candidatura(id_estado),
 data_alteracao DATE,
-observacao VARCHAR (100)
+observacao VARCHAR (100),
+
+FOREIGN KEY (id_candidatura) REFERENCES candidatura(id_candidatura),
+FOREIGN KEY (id_estado) REFERENCES estado_candidatura(id_estado)
 );
-
-
-
-OK
